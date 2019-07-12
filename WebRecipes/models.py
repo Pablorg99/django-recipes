88fa19f0ae7d44b85b671e32f84faf8256c021dd
Pablo Rodríguez Guillén <i72rogup@uco.es>
2019-07-12 12:33:29 +0200

    grams in displayed name and type hinting

--- WebRecipes/models.py
+++ WebRecipes/models.py
@@ -2,18 +2,18 @@ from django.db import models
 
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=50)
-    quantity = models.PositiveIntegerField('Grams')
+    name: str = models.CharField(max_length=50)
+    quantity: int = models.PositiveIntegerField('Grams')
 
     def __str__(self):
-        return self.name
-
+        return str(self.quantity) + "g" + self.name
+    
 
 class Recipe(models.Model):
-    name = models.CharField(max_length=100)
-    dinner_guests = models.PositiveIntegerField()
+    name: str = models.CharField(max_length=100)
+    dinner_guests: int = models.PositiveIntegerField()
     ingredients = models.ManyToManyField(Ingredient)
-    procedure = models.TextField('Instructions')
+    procedure: str = models.TextField('Instructions')
     publication_date = models.DateTimeField('Date Published')
 
     def __str__(self):
