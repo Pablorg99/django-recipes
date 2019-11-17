72582e9a838e59b9089e1c3c8c447a9ae7471677
Pablo Rodríguez Guillén <i72rogup@uco.es>
2019-11-17 19:02:49 +0100

    Django updated

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoRecipes.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django-recipes.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -13,7 +13,7 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        ) from exc
+        )
     execute_from_command_line(sys.argv)
 
 
